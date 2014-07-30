43ace95a5a0e578c5e4ae1e384bf2a47c2d4d016
Sergio Oliveira <sergio@tracy.com.br>
2014-07-30 18:33:41 -0300

    Updated gevent deps for redhat and centos

--- manifests/deps/gevent.pp
+++ manifests/deps/gevent.pp
@@ -1,4 +1,10 @@
 
 class appdeploy::deps::gevent {
-  ensure_packages(['libev-dev'])
+
+  case $operatingsystem {
+    'RedHat', 'CentOS': { $libev_dev = 'libev-dev' }
+    /^(Debian|Ubuntu)$/:{ $libev_dev = 'libev-devel' }
+  }
+  ensure_packages([$libev_dev])
+
 }
