21c8a3126bdbdbeea2714dc85c9ba34bc3cdae3b
Sergio Oliveira <sergio@tracy.com.br>
2014-07-30 18:33:26 -0300

    Updated essential for redhat family

--- manifests/deps/essential.pp
+++ manifests/deps/essential.pp
@@ -3,6 +3,18 @@ class appdeploy::deps::essential {
   ensure_packages([
     'git',
     'unzip',
-    'build-essential',
   ])
+
+  case $osfamily {
+    'redhat': {
+      exec { 'yum Group Install':
+        unless  => '/usr/bin/yum grouplist "Development tools" | /bin/grep "^Installed Groups"',
+        command => '/usr/bin/yum -y groupinstall "Development tools"',
+      }
+    }
+
+    'debian': {
+      ensure_packages(['build-essential'])
+    }
+  }
 }
