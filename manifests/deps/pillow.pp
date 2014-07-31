f30f17ede30698ed1f5589b615210067c9fa0f17
Sergio Oliveira <sergio@tracy.com.br>
2014-07-31 09:53:39 -0300

    Pillow deps requires puias repo on redhat family

--- manifests/deps/pillow.pp
+++ manifests/deps/pillow.pp
@@ -1,5 +1,8 @@
 
 class appdeploy::deps::pillow {
+
+  require puias
+
   case $osfamily {
     'RedHat': { ensure_packages(['libjpeg-devel', 'zlib-devel', 'freetype-devel']) }
     'Debian': { ensure_packages(['libjpeg-dev', 'zlib1g-dev', 'libfreetype6-dev']) }
