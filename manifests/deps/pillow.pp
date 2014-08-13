e50cdc54fd7cb8aa2469f87fef48d0066b523055
Sergio Oliveira <sergio@tracy.com.br>
2014-08-13 15:47:52 -0300

    Replaced libjpeg-devel by libjpeg-turbo-devel
    Only on CentOS

--- manifests/deps/pillow.pp
+++ manifests/deps/pillow.pp
@@ -4,7 +4,7 @@ class appdeploy::deps::pillow {
   require puias
 
   case $osfamily {
-    'RedHat': { ensure_packages(['libjpeg-devel', 'zlib-devel', 'freetype-devel']) }
+    'RedHat': { ensure_packages(['libjpeg-turbo-devel', 'zlib-devel', 'freetype-devel']) }
     'Debian': { ensure_packages(['libjpeg-dev', 'zlib1g-dev', 'libfreetype6-dev']) }
   }
 }
