57df10f57d2c77886594676338a909f80edb0ec4
Sergio Oliveira <sergio@tracy.com.br>
2014-09-16 14:05:06 -0300

    Creating gunicorn config file by default

--- manifests/django.pp
+++ manifests/django.pp
@@ -1,15 +1,24 @@
 
 define appdeploy::django (
   $user,
-  $ip = '127.0.0.1',
-  $port = 8001,
   $environment = undef,
   $celery = false,
   $proxy = true,
   $proxy_hosts = [],
   $directory = "/home/$user/$title/src",
-  $gunicorn_cfg = "$title/gunicorn.conf.py",
   $vhost_cfg_append = undef,
+  $gunicorn = {
+    workers      => 'auto',
+    worker_class => 'eventlet',
+    loglevel     => 'error',
+    bind         => '127.0.0.1:8001',
+    errorlog     => "~/$title.log",
+  },
+
+  # Deprecated params
+  $ip = undef,
+  $port = undef,
+  $gunicorn_cfg = undef,
 ) {
   include supervisor
 
@@ -24,11 +33,40 @@ define appdeploy::django (
     environment => $environment,
   }
 
+  validate_hash($gunicorn)
+
   $virtualenv_path = "/home/$user/.virtualenvs/$title"
   $manage_path = "$virtualenv_path/bin/python $directory/manage.py"
 
+  $default_gunicorn_cfg_path = "/etc/gunicorn-$title.conf.py"
+
+  $gunicorn_cfg_path = pick($gunicorn_cfg, '/etc/gunicorn-$title.conf.py')
+  if $gunicorn_cfg {
+    warning('Passing "gunicorn_cfg" to appdeploy::django is deprecated; please use the gunicorn parameter instead.')
+    $gunicorn_cfg_path = $gunicorn_cfg
+
+  } else {
+    $gunicorn_cfg_path = $default_gunicorn_cfg_path
+
+    file { "$title-gunicorn-config":
+      path    => $gunicorn_cfg_path,
+      content => template("appdeploy/django/gunicorn.conf.py.erb"),
+      before  => Supervisor::App[$title],
+    }
+  }
+
+  if $ip or $port {
+    # IP and Port should be set using $gunicorn hash map
+    warning('Passing "ip" and/or "port" to appdeploy::django is deprecated; please use gunicorn["bind"] parameter instead.')
+    $port_ = pick($port, '8001')
+    $ip_ = pick($ip, '127.0.0.1')
+    $app_cmd = "$virtualenv_path/bin/gunicorn $title.wsgi:application --bind=$ip_:$port_ --config=$gunicorn_cfg_path"
+  } else {
+    $app_cmd = "$virtualenv_path/bin/gunicorn $title.wsgi:application --config=$gunicorn_cfg_path"
+  }
+
   supervisor::app { $title:
-    command => "$virtualenv_path/bin/gunicorn $title.wsgi:application --bind=$ip:$port --config=$gunicorn_cfg",
+    command => $app_cmd,
   }
 
   if $celery {
